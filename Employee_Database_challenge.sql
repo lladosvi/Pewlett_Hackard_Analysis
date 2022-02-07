88fb23385f28f23affc1aef791bec98d3ea1a709
lladosvi <96096924+lladosvi@users.noreply.github.com>
2022-02-07 00:05:04 -0300

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -38,3 +38,26 @@ FROM mentor_eligibility as me
 GROUP BY me.title
 ORDER BY "count" DESC ;
 SELECT * FROM mentor_by_title;
+--Addition Analysis for Summary . This will be similar to the delivery 1 but intended to cover employees between years 55 and 65 that are assumed the remaining mentors.
+-- Deliverable Summary Steps 1-7
+SELECT e.emp_no, e.first_name,e.last_name,ti.title,ti.from_date,ti.to_date
+INTO non_retirement_or_mentoring_titles
+FROM employees as e
+JOIN titles as ti
+ON (e.emp_no=ti.emp_no)
+WHERE (e.birth_date BETWEEN '1955-01-01' AND '1964-12-31')
+ORDER BY e.emp_no, e.emp_no DESC ;
+SELECT * FROM non_retirement_or_mentoring_titles;
+-- Deliverable Summary Steps 8-15
+SELECT DISTINCT ON (rt2.emp_no) rt2.emp_no, rt2.first_name,rt2.last_name,rt2.title
+INTO unique2_titles
+FROM non_retirement_or_mentoring_titles as rt2
+WHERE (rt2.to_date = '9999-01-01');
+SELECT * FROM unique2_titles;
+-- Deliverable Summary Steps 16-21
+SELECT COUNT (ut2.emp_no), ut2.title
+INTO summary_titles
+FROM unique2_titles as ut2
+GROUP BY ut2.title
+ORDER BY "count" DESC ;
+SELECT * FROM summary_titles;
